8e8ff6452ec7db4c673984547a5d8151b9f51fed
Tom <tomboos@gmx.de>
2026-02-10 17:53:45 +0100

    feat(client): basic nav to raid page

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { NzButtonComponent } from 'ng-zorro-antd/button'
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [RouterLink, NzButtonComponent],
   templateUrl: './start.component.html',
 })
 export class StartComponent {}
