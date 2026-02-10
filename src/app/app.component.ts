d90a668440f91c9e928f5a9c08069927a1715110
Tom <tomboos@gmx.de>
2026-02-10 12:52:33 +0100

    feat(client): basic ng zorro setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NzInputDirective} from "ng-zorro-antd/input";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    FormsModule,
+    NzInputDirective
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'taktische-toolbox';
+  value: string = ""
 }
